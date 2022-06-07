ed05af2c9baeb96312cd0e6610ab4f7667bb828e
Ricky <a0935640996@gmail.com>
2022-06-07 15:28:37 +0800

    new

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <Slider />
+    </>
+  );
+}
+
+export default Home;
